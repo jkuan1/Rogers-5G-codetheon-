7059cd7aa228624e4c068036705a4d46dd53b2d7
m <mbe_pro@outlook.com>
2019-10-26 11:39:31 -0700

    Committing before trying to nuke the library and rebuild with gradle.

--- app/src/main/java/com/example/rogers5g/CameraActivity.java
+++ app/src/main/java/com/example/rogers5g/CameraActivity.java
@@ -1,23 +1,104 @@
 package com.example.rogers5g;
 
 import android.app.Activity;
+import android.content.Context;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.SparseIntArray;
+import android.view.Surface;
 import android.view.View;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.annotation.WorkerThread;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.common.FirebaseVisionImageMetadata;
+import com.google.firebase.ml.vision.face.FirebaseVisionFace;
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetector;
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetectorOptions;
 import com.otaliastudios.cameraview.CameraView;
 import com.otaliastudios.cameraview.filter.Filter;
 import com.otaliastudios.cameraview.filter.Filters;
 import com.otaliastudios.cameraview.filter.MultiFilter;
+import com.otaliastudios.cameraview.frame.Frame;
+import com.otaliastudios.cameraview.frame.FrameProcessor;
+import com.otaliastudios.cameraview.size.Size;
+
+import java.util.List;
 
 public class CameraActivity extends AppCompatActivity {
+    private FirebaseVisionFaceDetectorOptions options;
+    private FirebaseVisionFaceDetector detector;
+    private CameraActivity act;
     // For activities
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        options = new FirebaseVisionFaceDetectorOptions.Builder()
+                        .setPerformanceMode(FirebaseVisionFaceDetectorOptions.ACCURATE)
+                        .setLandmarkMode(FirebaseVisionFaceDetectorOptions.ALL_LANDMARKS)
+                        .setClassificationMode(FirebaseVisionFaceDetectorOptions.ALL_CLASSIFICATIONS)
+                        .build();
+
+        detector = FirebaseVision.getInstance().getVisionFaceDetector(options);
+        act = this;
+
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
         CameraView camera = findViewById(R.id.camera1);
         camera.setLifecycleOwner(this);
         camera.setFilter(new MultiFilter(Filters.TINT.newInstance()));
+        // https://natario1.github.io/CameraView/docs/frame-processing.html
+        camera.addFrameProcessor(new FrameProcessor() {
+            @Override
+            @WorkerThread
+            public void process(Frame frame) {
+                byte[] data = frame.getData();
+                int rotation = frame.getRotation();
+                long time = frame.getTime();
+                Size size = frame.getSize();
+                int format = frame.getFormat();
+
+                FirebaseVisionImageMetadata metadata = new FirebaseVisionImageMetadata.Builder()
+                        .setWidth(size.getWidth())
+                        .setHeight(size.getHeight())
+                        .setFormat(FirebaseVisionImageMetadata.IMAGE_FORMAT_NV21)
+                        .setRotation(FirebaseVisionImageMetadata.ROTATION_270)
+                        .build();
+
+                FirebaseVisionImage image = FirebaseVisionImage.fromByteArray(data, metadata);
+
+                Task<List<FirebaseVisionFace>> result =
+                        detector.detectInImage(image)
+                                .addOnSuccessListener(
+                                        new OnSuccessListener<List<FirebaseVisionFace>>() {
+                                            @Override
+                                            public void onSuccess(List<FirebaseVisionFace> faces) {
+                                                Log.println(Log.INFO,
+                                                        "tracedFaces",
+                                                        faces.toString());
+                                            }
+                                        })
+                                .addOnFailureListener(
+                                        new OnFailureListener() {
+                                            @Override
+                                            public void onFailure(@NonNull Exception e) {
+                                                Log.println(Log.ERROR,
+                                                        "faceERR",
+                                                        "Failed to read faces");
+                                            }
+                                        });
+            }
+        });
     }
+
 }
--- app/src/main/java/com/example/rogers5g/MainActivity.java
+++ app/src/main/java/com/example/rogers5g/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.rogers5g;
 
+import android.content.Intent;
 import android.graphics.Rect;
 import android.os.Bundle;
+import android.view.View;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -24,111 +27,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
     }
 
-    private void detectFaces(FirebaseVisionImage image) {
-        // [START set_detector_options]
-        FirebaseVisionFaceDetectorOptions options =
-                new FirebaseVisionFaceDetectorOptions.Builder()
-                        .setClassificationMode(FirebaseVisionFaceDetectorOptions.ACCURATE)
-                        .setLandmarkMode(FirebaseVisionFaceDetectorOptions.ALL_LANDMARKS)
-                        .setClassificationMode(FirebaseVisionFaceDetectorOptions.ALL_CLASSIFICATIONS)
-                        .setMinFaceSize(0.15f)
-                        .enableTracking()
-                        .build();
-        // [END set_detector_options]
-
-        // [START get_detector]
-        FirebaseVisionFaceDetector detector = FirebaseVision.getInstance()
-                .getVisionFaceDetector(options);
-        // [END get_detector]
-
-        // [START run_detector]
-        Task<List<FirebaseVisionFace>> result =
-                detector.detectInImage(image)
-                        .addOnSuccessListener(
-                                new OnSuccessListener<List<FirebaseVisionFace>>() {
-                                    @Override
-                                    public void onSuccess(List<FirebaseVisionFace> faces) {
-                                        // Task completed successfully
-                                        // [START_EXCLUDE]
-                                        // [START get_face_info]
-                                        for (FirebaseVisionFace face : faces) {
-                                            Rect bounds = face.getBoundingBox();
-                                            float rotY = face.getHeadEulerAngleY();  // Head is rotated to the right rotY degrees
-                                            float rotZ = face.getHeadEulerAngleZ();  // Head is tilted sideways rotZ degrees
-
-                                            // If landmark detection was enabled (mouth, ears, eyes, cheeks, and
-                                            // nose available):
-                                            FirebaseVisionFaceLandmark leftEar = face.getLandmark(FirebaseVisionFaceLandmark.LEFT_EAR);
-                                            if (leftEar != null) {
-                                                FirebaseVisionPoint leftEarPos = leftEar.getPosition();
-                                            }
-
-                                            // If classification was enabled:
-                                            if (face.getSmilingProbability() != FirebaseVisionFace.UNCOMPUTED_PROBABILITY) {
-                                                float smileProb = face.getSmilingProbability();
-                                            }
-                                            if (face.getRightEyeOpenProbability() != FirebaseVisionFace.UNCOMPUTED_PROBABILITY) {
-                                                float rightEyeOpenProb = face.getRightEyeOpenProbability();
-                                            }
-
-                                            // If face tracking was enabled:
-                                            if (face.getTrackingId() != FirebaseVisionFace.INVALID_ID) {
-                                                int id = face.getTrackingId();
-                                            }
-                                        }
-                                        // [END get_face_info]
-                                        // [END_EXCLUDE]
-                                    }
-                                })
-                        .addOnFailureListener(
-                                new OnFailureListener() {
-                                    @Override
-                                    public void onFailure(@NonNull Exception e) {
-                                        // Task failed with an exception
-                                        // ...
-                                    }
-                                });
-        // [END run_detector]
-    }
-
-    private void faceOptionsExamples() {
-        // [START mlkit_face_options_examples]
-        // High-accuracy landmark detection and face classification
-        FirebaseVisionFaceDetectorOptions highAccuracyOpts =
-                new FirebaseVisionFaceDetectorOptions.Builder()
-                        .setPerformanceMode(FirebaseVisionFaceDetectorOptions.ACCURATE)
-                        .setLandmarkMode(FirebaseVisionFaceDetectorOptions.ALL_LANDMARKS)
-                        .setClassificationMode(FirebaseVisionFaceDetectorOptions.ALL_CLASSIFICATIONS)
-                        .build();
-    }
-
-    private void processFaceList(List<FirebaseVisionFace> faces) {
-        // [START mlkit_face_list]
-        for (FirebaseVisionFace face : faces) {
-            Rect bounds = face.getBoundingBox();
-            float rotY = face.getHeadEulerAngleY();  // Head is rotated to the right rotY degrees
-            float rotZ = face.getHeadEulerAngleZ();  // Head is tilted sideways rotZ degrees
-
-            // If landmark detection was enabled (mouth, ears, eyes, cheeks, and
-            // nose available):
-            FirebaseVisionFaceLandmark leftEar = face.getLandmark(FirebaseVisionFaceLandmark.LEFT_EAR);
-            if (leftEar != null) {
-                FirebaseVisionPoint leftEarPos = leftEar.getPosition();
-            }
-
-            // If contour detection was enabled:
-            List<FirebaseVisionPoint> leftEyeContour =
-                    face.getContour(FirebaseVisionFaceContour.LEFT_EYE).getPoints();
-            List<FirebaseVisionPoint> upperLipBottomContour =
-                    face.getContour(FirebaseVisionFaceContour.UPPER_LIP_BOTTOM).getPoints();
-
-            // If face tracking was enabled:
-            if (face.getTrackingId() != FirebaseVisionFace.INVALID_ID) {
-                int id = face.getTrackingId();
-            }
-        }
-        // [END mlkit_face_list]
+    public void goToCamera(View view) {
+        Intent intent = new Intent(this, CameraActivity.class);
+        startActivity(intent);
     }
 }
