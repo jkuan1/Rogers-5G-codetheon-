266d26572cdaa95f288d7a20a561d5785ec49c6f
gbhatia <gagan3012@users.noreply.github.com>
2019-10-26 13:32:54 -0700

    Login activity

--- app/src/main/java/com/example/rogers5g/LoginActivity.java
+++ app/src/main/java/com/example/rogers5g/LoginActivity.java
@@ -2,8 +2,6 @@ package com.example.rogers5g;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
