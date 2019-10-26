9b60827e2927231ce1d30985866a410526318e5b
m <mbe_pro@outlook.com>
2019-10-26 13:36:02 -0700

    Abstracted out login bit to a new class.

--- app/src/main/java/com/example/rogers5g/CameraActivity.java
+++ app/src/main/java/com/example/rogers5g/CameraActivity.java
@@ -46,65 +46,18 @@ public class CameraActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         options = new FirebaseVisionFaceDetectorOptions.Builder()
-                        .setPerformanceMode(FirebaseVisionFaceDetectorOptions.ACCURATE)
-                        .setLandmarkMode(FirebaseVisionFaceDetectorOptions.ALL_LANDMARKS)
-                        .setClassificationMode(FirebaseVisionFaceDetectorOptions.ALL_CLASSIFICATIONS)
-                        .enableTracking()
-                        .build();
+                .setPerformanceMode(FirebaseVisionFaceDetectorOptions.ACCURATE)
+                .setLandmarkMode(FirebaseVisionFaceDetectorOptions.ALL_LANDMARKS)
+                .setClassificationMode(FirebaseVisionFaceDetectorOptions.ALL_CLASSIFICATIONS)
+                .enableTracking()
+                .build();
 
         detector = FirebaseVision.getInstance().getVisionFaceDetector(options);
         act = this;
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
-        CameraView camera = findViewById(R.id.camera);
-        camera.setLifecycleOwner(this);
-        // https://natario1.github.io/CameraView/docs/frame-processing.html
-        camera.addFrameProcessor(new FrameProcessor() {
-            @Override
-            @WorkerThread
-            public void process(Frame frame) {
-                byte[] data = frame.getData();
-                int rotation = frame.getRotation();
-                long time = frame.getTime();
-                Size size = frame.getSize();
-                int format = frame.getFormat();
-
-                FirebaseVisionImageMetadata metadata = new FirebaseVisionImageMetadata.Builder()
-                        .setWidth(size.getWidth())
-                        .setHeight(size.getHeight())
-                        .setFormat(FirebaseVisionImageMetadata.IMAGE_FORMAT_NV21)
-                        .setRotation(FirebaseVisionImageMetadata.ROTATION_270)
-                        .build();
-
-                FirebaseVisionImage image = FirebaseVisionImage.fromByteArray(data, metadata);
-
-                Task<List<FirebaseVisionFace>> result =
-                        detector.detectInImage(image)
-                                .addOnSuccessListener(
-                                        new OnSuccessListener<List<FirebaseVisionFace>>() {
-                                            @Override
-                                            public void onSuccess(List<FirebaseVisionFace> faces) {
-                                                lastFaces = faces;
-                                                Log.println(Log.INFO,
-                                                        "tracedFaces",
-                                                        faces.toString());
-                                            }
-                                        })
-                                .addOnFailureListener(
-                                        new OnFailureListener() {
-                                            @Override
-                                            public void onFailure(@NonNull Exception e) {
-                                                Log.println(Log.ERROR,
-                                                        "faceERR",
-                                                        "Failed to read faces");
-                                            }
-                                        });
-            }
-        });
-    }
-
-    public List<FirebaseVisionFace> getFaces() {
-        return lastFaces;
+        GetAndOverlay child = new GetAndOverlay();
+        child.runCamera(this, this);
     }
 }
---
+++ app/src/main/java/com/example/rogers5g/GetAndOverlay.java
@@ -0,0 +1,99 @@
+package com.example.rogers5g;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.WorkerThread;
+import androidx.lifecycle.LifecycleOwner;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.common.FirebaseVisionImageMetadata;
+import com.google.firebase.ml.vision.face.FirebaseVisionFace;
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetector;
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetectorOptions;
+import com.otaliastudios.cameraview.CameraView;
+import com.otaliastudios.cameraview.frame.Frame;
+import com.otaliastudios.cameraview.frame.FrameProcessor;
+import com.otaliastudios.cameraview.size.Size;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GetAndOverlay {
+    private FirebaseVisionFaceDetectorOptions options;
+    private FirebaseVisionFaceDetector detector;
+    private Activity act;
+    private List<FirebaseVisionFace> lastFaces;
+
+    // Access last seen face w/ getFaces
+    protected void runCamera(Activity parentActivity, LifecycleOwner lifeCycleOwner) {
+        lastFaces = new ArrayList<>();
+        options = new FirebaseVisionFaceDetectorOptions.Builder()
+                .setPerformanceMode(FirebaseVisionFaceDetectorOptions.ACCURATE)
+                .setLandmarkMode(FirebaseVisionFaceDetectorOptions.ALL_LANDMARKS)
+                .setClassificationMode(FirebaseVisionFaceDetectorOptions.ALL_CLASSIFICATIONS)
+                .enableTracking()
+                .build();
+
+        detector = FirebaseVision.getInstance().getVisionFaceDetector(options);
+        act = parentActivity;
+
+        CameraView camera = parentActivity.findViewById(R.id.camera);
+        camera.setLifecycleOwner(lifeCycleOwner);
+        // https://natario1.github.io/CameraView/docs/frame-processing.html
+        camera.addFrameProcessor(new FrameProcessor() {
+            @Override
+            @WorkerThread
+            public void process(Frame frame) {
+                byte[] data = frame.getData();
+                int rotation = frame.getRotation();
+                long time = frame.getTime();
+                Size size = frame.getSize();
+                int format = frame.getFormat();
+
+                FirebaseVisionImageMetadata metadata = new FirebaseVisionImageMetadata.Builder()
+                        .setWidth(size.getWidth())
+                        .setHeight(size.getHeight())
+                        .setFormat(FirebaseVisionImageMetadata.IMAGE_FORMAT_NV21)
+                        .setRotation(FirebaseVisionImageMetadata.ROTATION_270)
+                        .build();
+
+                FirebaseVisionImage image = FirebaseVisionImage.fromByteArray(data, metadata);
+
+                Task<List<FirebaseVisionFace>> result =
+                        detector.detectInImage(image)
+                                .addOnSuccessListener(
+                                        new OnSuccessListener<List<FirebaseVisionFace>>() {
+                                            @Override
+                                            public void onSuccess(List<FirebaseVisionFace> faces) {
+                                                if (faces.size() != 0) {
+                                                    lastFaces = faces;
+                                                    Log.println(Log.INFO,
+                                                            "tracedFaces",
+                                                            lastFaces.toString());
+                                                }
+                                            }
+                                        })
+                                .addOnFailureListener(
+                                        new OnFailureListener() {
+                                            @Override
+                                            public void onFailure(@NonNull Exception e) {
+                                                Log.println(Log.ERROR,
+                                                        "faceERR",
+                                                        "Failed to read faces");
+                                            }
+                                        });
+            }
+        });
+    }
+
+    public List<FirebaseVisionFace> getFaces() {
+        return lastFaces;
+    }
+}
